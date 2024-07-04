4ce030328315f0220f5d627e31182aa682c9af3a
Ivan Starčević <ivan.starcevic15@gmail.com>
2024-07-04 16:13:41 +0200

    use modern if and else

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
-import { NgForOf } from '@angular/common';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, NgForOf],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = signal('');
+  selectedUser = computed(() =>
+    this.users.find(user => user.id === this.selectedUserId())
+  );
 
   onSelectUser(id: string) {
-    console.log(`User selected: ${id}`);
+    this.selectedUserId.set(id);
   }
 }
